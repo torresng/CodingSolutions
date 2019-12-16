8d8a85a311fcc50a681e3cc3f9fc51db4830a941
Torres Ng <torresng2684cte@gmail.com>
2019-12-16 14:06:40 +0800

    0046

---
+++ 0046_二叉搜索树的后序遍历序列/main.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    vector<int> seq;
+
+    bool verifySequenceOfBST(vector<int> sequence) {
+        seq = sequence;
+        if (seq.empty()) {
+            return true;
+        }
+        return dfs(0, seq.size() - 1);
+    }
+
+    bool dfs(int l, int r) {
+        if (l >= r) {
+            return true;
+        }
+        int root = seq[r];
+        int k = l;
+        while (k < r && seq[k] < root) {
+            k++;
+        }
+        for (int i = k; i < r; i++) {
+            if (seq[i] < root) {
+                return false;
+            }
+        }
+        return dfs(l, k - 1) && dfs(k, r - 1);
+    }
+};
