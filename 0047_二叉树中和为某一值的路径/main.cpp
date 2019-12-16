66a16e207f4fbb424b1516ff5de8ebbef0aa5135
Torres Ng <torresng2684cte@gmail.com>
2019-12-16 14:06:47 +0800

    0047

---
+++ 0047_二叉树中和为某一值的路径/main.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    vector<vector<int>> ans;
+    vector<int> path;
+
+    vector<vector<int>> findPath(TreeNode *root, int sum) {
+        dfs(root, sum);
+        return ans;
+    }
+
+    void dfs(TreeNode *root, int sum) {
+        if (!root) {
+            return;
+        }
+        path.push_back(root->val);
+        sum -= root->val;
+        if (!root->left && !root->right && !sum) {
+            ans.push_back(path);
+        }
+        dfs(root->left, sum);
+        dfs(root->right, sum);
+        path.pop_back();
+    }
+};
