b92186c0b09e07047a23139cb56e97a2bf560a1d
Torres Ng <torresng2684cte@gmail.com>
2020-01-08 13:08:31 +0800

    0078

---
+++ 0078_左旋转字符串/main.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    string leftRotateString(string str, int n) {
+        reverse(str.begin(), str.end());
+        reverse(str.begin(), str.begin() + str.size() - n);
+        reverse(str.begin() + str.size() - n, str.end());
+        return str;
+    }
+};
